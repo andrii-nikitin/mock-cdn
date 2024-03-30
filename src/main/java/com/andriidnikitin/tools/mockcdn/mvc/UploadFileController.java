4353f2606b0ceb91ab16966ed44a489b5f4ef43d
andrii-nikitin <68604409+andrii-nikitin@users.noreply.github.com>
2024-03-30 22:48:38 +0200

    updated file size limits

--- src/main/java/com/andriidnikitin/tools/mockcdn/mvc/UploadFileController.java
+++ src/main/java/com/andriidnikitin/tools/mockcdn/mvc/UploadFileController.java
@@ -6,7 +6,6 @@ import java.io.IOException;
 import java.util.List;
 import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Profile;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,7 +14,6 @@ import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
-@Profile("!functionaltest")
 public class UploadFileController {
 
   private final StorageService storageService;
